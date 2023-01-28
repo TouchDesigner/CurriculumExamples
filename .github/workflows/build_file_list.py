9e109748b6325de620558d30e56e4ba0bd20385b
Matthew Ragan <raganmd@gmail.com>
2023-01-28 11:47:03 -0800

    Action testing

--- .github/workflows/build_file_list.py
+++ .github/workflows/build_file_list.py
@@ -30,6 +30,8 @@ new_release_paths = [
     'toxExamples/sweet16',
     'toxExamples/TouchDesignerFundamentals']
 
+print("->Starting automated file generation")
+
 # NOTE - creates download manifest
 # create download manifest
 for each_release_path in new_release_paths:
@@ -41,9 +43,10 @@ for each_release_path in new_release_paths:
     for each_manifest in url_manifests:
         if os.path.isfile(each_manifest):
             os.remove(each_manifest)
+            print(f"-->removing file {each_manifest}")
 
     # generate tox download manifest
-    print(f"creating manifest {manifest}")
+    print(f"--->creating manifest {manifest}")
     with open(manifest, 'w') as manifest_file:
         for root, dirs, files in os.walk(each_release_path):
             for each_file in files:
@@ -57,7 +60,7 @@ for each_release_path in new_release_paths:
                     manifest_file.write(f'{tox_url}\n')
 
     # generate curriculum links
-    print(f"creating curriculum links {curriculum_links}")    
+    print(f"--->creating curriculum links {curriculum_links}")    
     with open(curriculum_links, 'w') as curriculum_links_file:
         for root, dirs, files in os.walk(each_release_path):
             for each_file in files:
@@ -74,3 +77,4 @@ for each_release_path in new_release_paths:
                     curriculum_links_file.write(f'{tox_url}\n')
                     curriculum_links_file.write('\n')
 
+print("Automated file generation completed")
