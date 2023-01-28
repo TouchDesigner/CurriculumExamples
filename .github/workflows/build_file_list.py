3cb0a6845b4c31dc6c4730666d911507a2ab6d0b
Matthew Ragan <raganmd@gmail.com>
2023-01-28 11:20:03 -0800

    Changes to approach to avoid failed git action
    The git action that runs will fail if there are no changes in the repo. This will remove the previous manifest file before re-creating it - which should resolve that possible failed action.

--- .github/workflows/build_file_list.py
+++ .github/workflows/build_file_list.py
@@ -1,9 +1,27 @@
+# @Author: Matthew Ragan
+# @Date: 01.28.2023
+# @Email: matthew@sudomagic.com
+
+'''
+This script acts as an automated means of generating a link list to be used
+in TouchDesigner curriculum pages. The automation here is intended to automate
+the process of generating links to reduce errors when copying and pasting.
+
+curriculum_links is a file generated for faster integration with the
+learn.derivative site
+
+url_manifests is a file that can be used to download all files from
+an example set for a local store.
+'''
+
 import os
 
-#NOTE - download_link_struct
+# NOTE - repo variables
 repo_owner = "TouchDesigner"
 repo_name = "CurriculumExamples"
 branch_name = "main"
+
+#NOTE - download_link_structs
 link_struct = "https://github.com/{repo_owner}/{repo_name}/raw/{branch_name}/{asset_path}"
 navigator_link_struct = "?actionable=1&action=load_tox&remotePath={url}"
 
@@ -17,7 +35,13 @@ new_release_paths = [
 for each_release_path in new_release_paths:
     manifest = f"{each_release_path}/manifest.txt"
     curriculum_links = f"{each_release_path}/curriculum_links.txt"
-    
+    url_manifests = [manifest, curriculum_links]
+
+    # delete manifest and curriculum if they exist
+    for each_manifest in url_manifests:
+        if os.path.isfile(each_manifest):
+            os.remove(each_manifest)
+
     # generate tox download manifest
     print(f"creating manifest {manifest}")
     with open(manifest, 'w') as manifest_file:
